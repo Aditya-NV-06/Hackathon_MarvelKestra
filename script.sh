07f01feea1a7193ea0432cd8fb3a10e9b0fe78b7
Aditya-NV-06 <adityanv4@gmail.com>
2024-11-30 21:55:14 +0530

    script to run

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter 1 to build the image and 0 to stop the container"
+read -p "Enter your choice: " choice
+if [ $choice -eq 1 ]; then
+   
+   sudo docker compose up -d
+elif [ $choice -eq 0 ]; then
+    sudo docker compose down
+else
+    echo "Invalid input"
+fi
