e81f069546e4b2de0fd5514e293ff932acb37a9b
Aditya-NV-06 <adityanv4@gmail.com>
2024-11-30 22:06:01 +0530

    fix: improve script formatting and add exit status

--- script.sh
+++ script.sh
@@ -3,10 +3,12 @@
 echo "Enter 1 to build the image and 0 to stop the container"
 read -p "Enter your choice: " choice
 if [ $choice -eq 1 ]; then
-   
-   sudo docker compose up -d
+    sudo docker compose up -d
 elif [ $choice -eq 0 ]; then
-    sudo docker compose down
+     sudo docker compose down
 else
-    echo "Invalid input"
+     echo "Invalid input"
 fi
+exit 0
+
+
